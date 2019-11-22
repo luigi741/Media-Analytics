af60fcd59611de3d32f328e7ab1e7102527a6fde
mayo17 <45338543+mayo17@users.noreply.github.com>
2019-11-22 04:13:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/droptweets', (req, res) => {
 
 app.get('/createtweets', (req, res) => {
 	console.log('GET /createtweets');
-	var pQuery = 'CREATE TABLE tweets(user varchar(255), url varchar(255) PRIMARY KEY, hashtag varchar(255), score numeric(2,1), desc varchar(255));';
+	var pQuery = 'CREATE TABLE tweets( user varchar(255), url varchar(255) PRIMARY KEY, hashtag varchar(255), score numeric(2,1), desc varchar(255) );';
 	
 	pool.query(pQuery, (err, result) => {
 		if (err) {
